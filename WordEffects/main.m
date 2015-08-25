65d822bf42d8c18722209643eeb9019c4335935b
katiepeterso <katiepeterso@outlook.com>
2015-08-24 17:54:43 -0700

    Modifying loop to switch statement

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -30,53 +30,56 @@ int main(int argc, const char * argv[]) {
             
             //Remove new line from the end of inputString
             NSString *trimmedString = [inputString stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-            NSString *operation = [operationWithNewLine stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-            if ([operation isEqualToString:@"1"]) {
-                //Operation 1
-                NSString *uppercaseString = [inputString uppercaseString];
-                NSLog(@"%@", uppercaseString);
-            }
-            else if ([operation isEqualToString:@"2"]) {
-                //Operation 2
-                NSString *lowercaseString = [inputString lowercaseString];
-                NSLog(@"%@", lowercaseString);
-            }
-            else if ([operation isEqualToString:@"3"]) {
-                //Operation 3
-                NSNumberFormatter *nf = [[NSNumberFormatter alloc] init];
-                nf.numberStyle = NSNumberFormatterDecimalStyle;
-                NSNumber *numberObj = [nf numberFromString:trimmedString];
-                
-                if (numberObj) {
-                    int numericValue = [inputString intValue];
-                    NSLog(@"%d", numericValue);
-                } else {
-                    NSLog(@"You have to input a number to use this operation.");
+            NSString *trimmedOperation = [operationWithNewLine stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            int operation = [trimmedOperation intValue];
+            
+            switch (operation) {
+                case 1: {
+                    NSString *uppercaseString = [inputString uppercaseString];
+                    NSLog(@"%@", uppercaseString);
                 }
-                
-            }
-            else if ([operation isEqualToString:@"4"]) {
-                //Operation 4
-                NSString *canadianString = [trimmedString stringByAppendingFormat:@", eh?"];
-                NSLog(@"%@", canadianString);
-            }
-            else if ([operation isEqualToString:@"5"]) {
-                //Operation 5
-                if ([trimmedString hasSuffix:@"?"]) {
-                    NSLog(@"I don't know.");
+                    break;
+                case 2: {
+                    NSString *lowercaseString = [inputString lowercaseString];
+                    NSLog(@"%@", lowercaseString);
                 }
-                else if ([trimmedString hasSuffix:@"!"]) {
-                    NSLog(@"Whoa, calm down.");
-                }        }
-            else if ([operation isEqualToString:@"6"]) {
-                //Operation 6
-                NSString *noSpaces = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
-                NSLog(@"%@", noSpaces);
+                    break;
+                case 3: {
+                    NSNumberFormatter *nf = [[NSNumberFormatter alloc] init];
+                    nf.numberStyle = NSNumberFormatterDecimalStyle;
+                    NSNumber *numberObj = [nf numberFromString:trimmedString];
+                    
+                    if (numberObj) {
+                        int numericValue = [inputString intValue];
+                        NSLog(@"%d", numericValue);
+                    } else {
+                        NSLog(@"You have to input a number to use this operation.");
+                    }
+                }
+                    break;
+                case 4: {
+                    NSString *canadianString = [trimmedString stringByAppendingFormat:@", eh?"];
+                    NSLog(@"%@", canadianString);
+                }
+                    break;
+                case 5: {
+                    if ([trimmedString hasSuffix:@"?"]) {
+                        NSLog(@"I don't know.");
+                    }
+                    else if ([trimmedString hasSuffix:@"!"]) {
+                        NSLog(@"Whoa, calm down.");
+                    }
+                }
+                    break;
+                case 6:{
+                    NSString *noSpaces = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+                    NSLog(@"%@", noSpaces);
+                }
+                    break;
+                default: NSLog(@"You must enter a number 1-6 to perform an operation");
+                    break;
             }
-            else {
-                NSLog(@"You must enter a number 1-6 to perform an operation");
-            }
-
+            
         }
         
     }
