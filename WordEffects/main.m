b4056d51dd86da8383f61a9f58c70cf6e3d23595
katiepeterso <katiepeterso@outlook.com>
2015-08-24 17:37:49 -0700

    Adding looping to allow user to continually perform new string operations

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -12,58 +12,73 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // 255 unit long array of characters
         char inputChars[255];
-        
-        printf("Input a string: ");
-        // limit input to max 255 characters
-        fgets(inputChars, 255, stdin);
-        
-        // print as a c string
-        printf("Your string is %s", inputChars);
-        
-        // convert char array to an NSString object
-        NSString *inputString = [NSString stringWithUTF8String:inputChars];
-        
-        // print NSString object
-        NSLog(@"Input was: %@", inputString);
-        
-        //Remove new line from the end of inputString
-        NSString *trimmedString = [inputString stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-        
-        //Operation 1
-        NSString *uppercaseString = [inputString uppercaseString];
-        NSLog(@"%@", uppercaseString);
-        
-        //Operation 2
-        NSString *lowercaseString = [inputString lowercaseString];
-        NSLog(@"%@", lowercaseString);
-        
-        //Operation 3
-        NSNumberFormatter *nf = [[NSNumberFormatter alloc] init];
-        nf.numberStyle = NSNumberFormatterDecimalStyle;
-        NSNumber *numberObj = [nf numberFromString:trimmedString];
-        
-        if (numberObj) {
-            int numericValue = [inputString intValue];
-            NSLog(@"%d", numericValue);
-        } else {
-            NSLog(@"You have to input a number to use this operation.");
-        }
-        
-        //Operation 4
+        char inputNumber[255];
+        for (;;) {
+            printf("Input a string: ");
+            // limit input to max 255 characters
+            fgets(inputChars, 255, stdin);
+            
+            printf("Input an operation number 1-6: ");
+            fgets(inputNumber, 255, stdin);
+            
+            // convert char arrays to an NSString objects
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            NSString *operationWithNewLine = [NSString stringWithUTF8String:inputNumber];
+            
+            // print NSString object
+            NSLog(@"Input was: %@ for operation: %@", inputString, operationWithNewLine);
+            
+            //Remove new line from the end of inputString
+            NSString *trimmedString = [inputString stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            NSString *operation = [operationWithNewLine stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            if ([operation isEqualToString:@"1"]) {
+                //Operation 1
+                NSString *uppercaseString = [inputString uppercaseString];
+                NSLog(@"%@", uppercaseString);
+            }
+            else if ([operation isEqualToString:@"2"]) {
+                //Operation 2
+                NSString *lowercaseString = [inputString lowercaseString];
+                NSLog(@"%@", lowercaseString);
+            }
+            else if ([operation isEqualToString:@"3"]) {
+                //Operation 3
+                NSNumberFormatter *nf = [[NSNumberFormatter alloc] init];
+                nf.numberStyle = NSNumberFormatterDecimalStyle;
+                NSNumber *numberObj = [nf numberFromString:trimmedString];
+                
+                if (numberObj) {
+                    int numericValue = [inputString intValue];
+                    NSLog(@"%d", numericValue);
+                } else {
+                    NSLog(@"You have to input a number to use this operation.");
+                }
+                
+            }
+            else if ([operation isEqualToString:@"4"]) {
+                //Operation 4
                 NSString *canadianString = [trimmedString stringByAppendingFormat:@", eh?"];
-        NSLog(@"%@", canadianString);
-        
-        //Operation 5
-        if ([trimmedString hasSuffix:@"?"]) {
-            NSLog(@"I don't know.");
+                NSLog(@"%@", canadianString);
+            }
+            else if ([operation isEqualToString:@"5"]) {
+                //Operation 5
+                if ([trimmedString hasSuffix:@"?"]) {
+                    NSLog(@"I don't know.");
+                }
+                else if ([trimmedString hasSuffix:@"!"]) {
+                    NSLog(@"Whoa, calm down.");
+                }        }
+            else if ([operation isEqualToString:@"6"]) {
+                //Operation 6
+                NSString *noSpaces = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+                NSLog(@"%@", noSpaces);
+            }
+            else {
+                NSLog(@"You must enter a number 1-6 to perform an operation");
+            }
+
         }
-        else if ([trimmedString hasSuffix:@"!"]) {
-            NSLog(@"Whoa, calm down.");
-        }
         
-        //Operation 6
-        NSString *noSpaces = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
-        NSLog(@"%@", noSpaces);
     }
     return 0;
 }
